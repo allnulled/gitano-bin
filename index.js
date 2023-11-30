bd257afeda022dc99b5411c1db870815902e18d0
allnulled <todosanulados@gmail.com>
2023-11-30 23:28:46 +0100

    default commit message

---
+++ index.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+const [bin_node, bin_gitano, comando, ...argumentos] = process.argv;
+
+console.log("[*] Using «gitano» powerful command line utility [*]");
+console.log("[*] Gitano path: " + bin_gitano);
+console.log("[*] Node path: " + bin_node);
+console.log("[*] Gitano command: " + comando);
+console.log("[*] Current directory:  " + process.cwd());
+
+const cmd_options = {
+    stdio: [process.stdin, process.stdout, process.stderr],
+    cwd: process.cwd()
+};
+const comando_push = function(mensaje) {
+    const child_process = require("child_process");
+    child_process.execSync("git init .", cmd_options);
+    child_process.execSync("git add .", cmd_options);
+    child_process.execSync("git commit . -m " + JSON.stringify(mensaje), cmd_options);
+    child_process.execSync("git push", cmd_options);
+};
+const comando_versionate = function(mensaje) {
+    const child_process = require("child_process");
+    const path = require("path");
+    child_process.execSync("git init .", cmd_options);
+    child_process.execSync("git add .", cmd_options);
+    child_process.execSync("git commit . -m " + JSON.stringify(mensaje), cmd_options);
+    child_process.execSync("git push", cmd_options);
+    child_process.execSync("npm version patch", cmd_options);
+    child_process.execSync("git add .", cmd_options);
+    const version = require(path.resolve(process.cwd(), "package.json")).version;
+    child_process.execSync("git commit . -m " + JSON.stringify("v" + version), cmd_options);
+    child_process.execSync("git push", cmd_options);
+
+};
+
+if(typeof comando === "undefined") {
+    throw new Error("Required parameter «command» in order to execute «gitano»");
+}
+let mensaje = "";
+for(let index=0; index<argumentos.length; index++) {
+  const argumento = argumentos[index];
+  mensaje += argumento;
+  mensaje += " ";
+}
+mensaje = mensaje.trim();
+if(mensaje === "") {
+    mensaje = "default commit message";
+}
+if(comando === "push") {
+    comando_push(mensaje);
+} else if(comando === "versionate") {
+    comando_versionate(mensaje);
+} else {
+    throw new Error("Required parameter «command» to be valid («push» or «versionate») in order to execute «gitano»");
+}
